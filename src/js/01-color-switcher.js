d233027d8d8c61132a250abf2e166ec7b376fd83
Mamba <113925095+mrs-ktulhu@users.noreply.github.com>
2023-03-24 14:42:09 +0000

    sketches

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
+  
+  const bgdColorBtnStart = document.querySelector('button[data-start]');
+  const bgdColorBtnStop = document.querySelector('button[data-stop]');
+  
+  bgdColorBtnStart.addEventListener('click', onStartBtnClick);
+  bgdColorBtnStop.addEventListener('click', onStopBtnClick);
+  
+  let timerId = null;
+  
+  function onStartBtnClick() {
+    bgdColorBtnStart.disabled = true;
+    bgdColorBtnStop.disabled = false;
+    timerId = setInterval(setColor, 1000);
+  }
+  
+  function setColor() {
+    const colorValue = getRandomHexColor();
+    //   console.log(`Color = `, colorValue);
+    document.body.style.backgroundColor = colorValue;
+  }
+  
+  function onStopBtnClick() {
+    bgdColorBtnStart.disabled = false;
+    bgdColorBtnStop.disabled = true;
+    clearInterval(timerId);
+  }
